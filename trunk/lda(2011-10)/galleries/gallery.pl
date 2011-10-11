5d67c3cfc504aebafd02d1ace00470be5ed29262
thomasgaylard2 <thomasgaylard2@5a7787fc-1411-42a9-8abd-d5ce809162c2>
2011-10-11 09:06:25 +0000

    [#2] Island of Stability: Have script which runs in a directory and can create a gallery page using the images and thumbnails in a named subdirectory, and using a template.
    Next steps are:
    
    - Loop over all sub directories (one for each gallery)
    - Extend to create non-gallery pages using text files with markdown syntax text.

--- trunk/lda(2011-10)/galleries/gallery.pl
+++ trunk/lda(2011-10)/galleries/gallery.pl
@@ -4,135 +4,128 @@ use warnings;
 use feature ':5.10';
 use HTML::Template;
 
-# Template Handling
-my $template = HTML::Template->new(filename => 'gallery_template.html');
+sub get_image_data {
+    # Takes the pathname of a directory (or looks in current directory) and creates a list of all image
+    # files in the directory together with the height and width of each image
+    
+    my $path = shift;
+    if (!defined $path) {
+        $path = ".";
+    }
+        
+    my $image_data = `cd $path;sips -g pixelWidth -g pixelHeight *.jpg *.JPG`;
+    
+    my $line;
+    my $name;
+    
+    my @image_array;
+    my @single_image_properties;
+    
+    open my $handle, '<', \$image_data;
+    while (defined ($line = <$handle>)) {
+        # Lines will appear in threes because of the way the sips command is constructed :
+        # - pathname
+        # - width
+        # - height
+        chomp $line;
+        # Initialise image propoerties so we can tell whether they have all been set later
+        say $line;
+        
+        if ($line =~ m/^\/.+\/(.+\.(jpg|jpeg|JPG|JPEG))/) {
+            @single_image_properties = ();
+            $single_image_properties[0] = $1;
+            say "single image properties [0] is $single_image_properties[0]";
+        } elsif ($line =~ m/^  pixelWidth: (\d+)/) {
+            $single_image_properties[1] = $1;
+            say "single image properties [1] is $single_image_properties[1]";
+        } elsif ($line =~ m/^  pixelHeight: (\d+)/) {
+            $single_image_properties[2] = $1;
+            say "single image properties [2] is $single_image_properties[2]";
+            if ((!(exists $single_image_properties[0])) || (!(exists $single_image_properties[1]))) {
+                die "Incomplete data while processing sips command, current line is $line";
+            } else {
+                say "length of single_image_properties is " . scalar(@single_image_properties);
+                push (@image_array, [@single_image_properties]);
+            }
+        }
+    }
+    say "length of image_array is " . scalar(@image_array);
+    @image_array;
+}
+
+sub create_table_of_images {
+    # Takes array of image data and creates a table for use in a gallery page. Assumes
+    # that thumbnails exist for each file with prefix TN_ in sub dir called thumbnails.
+    # TD elements will be set by css, but images will need to be set individually to
+    # get orientation and aspect ration correct.
+    #
+    
+    my($image_array, $max_dimension, $image_path, $table_column_width) = @_;
+    
+    say "Number of elements in image array (in sub) is " . scalar(@$image_array);
+    say "max dimension is $max_dimension";
+    say "image path is $image_path";
+    say "table_column_width is $table_column_width";
+
+    my $image_data;
+    my $size_string;
+    my $tag_image;
+    my $thumbnail_path;
+    my $html_line;
+    my $html;
+    my $output_lines;
+
+    $html = "";
+    $output_lines = 0;
+    foreach $image_data (@$image_array) {
+        my ($name, $height, $width) = @$image_data;
+        
+        say "Current name is $name";
+        
+        $thumbnail_path = $image_path . "/thumbnails/" . $name;
+        if ($height >= $width) {
+            $size_string = "height='" . $max_dimension . "'";
+        } else {
+            $size_string = "width='" . $max_dimension . "'";
+        }
+
+        $tag_image = "<img " . " src='$image_path/$name' $size_string" . " />";
+        $html_line = "<td>" . "<a " . "href='$thumbnail_path'" . ">" . $tag_image . "</a>" . "</td>";
+        say "Current line is : $html_line";
+        
+        if ($output_lines == 0) {
+            $html .= "<table class='gallery'" . "\n";
+        }
+        if ($output_lines % $table_column_width == 0) {
+            if ($output_lines != 0) {
+                $html .= "</tr>" . "\n";
+            }
+            $html .= "<tr>" . "\n";
+        }
+        $html .= $html_line . "\n";
+
+        $output_lines++;
+    }
+    $html .= ("<td></td>" . "\n") x ($table_column_width - ($output_lines % $table_column_width));
+    $html .= "</tr>\n</table>";
+    $html;
+}
 
 # Formatting parameters
 my $table_column_width = 4;
 my $max_dimension = 130;
+my $image_path = "gallery-01";
 
-# Tags used to construct HTML - table related
-my $tag_table_left = "<table class='gallery'>";
-my $tag_table_right = "</table>";
-my $tag_tr_left = "<tr>";
-my $tag_tr_right ="</tr>";
-my $tag_td_left = "<td>";
-my $tag_td_right = "</td>";
+# Process image files in 
+my @images = get_image_data($image_path);
 
-# Tags for links and images
-my $tag_anchor_left = "<a>";
-my $tag_anchor_right = "</a>";
-my $tag_img_left = "<img>";
-my $tag_img_right = "</img>";
+# Create gallery table
+my $page = create_table_of_images(\@images, 130, $image_path, 4, $image_path);
 
-# HTML working strings
-my $tag_image;
-my $href;
-my $html_line;
-my $size_string;
-
-# Data used to construct HTML
-my $image_name;
-my $thumbnail_name;
-my $image_width;
-my $image_height;
-
-my $line;
-
-my $line_count = 0;
-my $output_lines = 0;
-my $output_text;
-my $process_flag;
-
-# If there is a parameter it will be the path to the folder where the images are
-
-my $image_path;
-if (defined $ARGV[0]) {
-    $image_path = $ARGV[0];
-} else {
-    $image_path = ".";
-}
-
-# Get list of image files with width and height.  This will probably only work on Mac or Unix/Linux platform
-# Replace with standard Perl library in future.
-my $sips_command = "sips -g pixelWidth -g pixelHeight *.jpg *.JPG";
-my $image_data = `$sips_command`;
-
-# Open file to write HTML snippet to
-open my $handle, '<', \$image_data;
-my $write=""; # String used to create output data
-
-# Process output from sips command.  For every thumbnail, create the html line but check whether main image file
-# exists first.  Output list of files where there is a thumbnail but the main file doesn't exist.
-$process_flag = 0;
-while (defined ($line = <$handle>)) {
-    $line_count++;
-    chomp $line;
-    if ($line =~ m/^\/.+\/(.+\.(jpg|jpeg|JPG|JPEG))/) {
-        
-        # This line represents a file, so check whether we want to process it.
-        # Only process if this is a thumbnail (as there is nothing to display on the link otherwise)
-        
-        $thumbnail_name = $1;
-        if ($thumbnail_name =~ m/TN_(.+)/) {
-
-            # This is a thumbnail - check whether main file exists and skip if not.
-            if (!-e $1) {
-                $process_flag = 0;
-            } else {
-                $process_flag = 1;
-                $image_name = $1;
-                $href = "href='$image_path/$image_name'";
-            }
-        } else {
-            # Not a thumbnail so skip
-            $process_flag = 0;
-        }
-    } elsif ($line =~ m/^  pixelWidth: (\d+)/) {
-        
-        # This line is for the image width, so check we are processing the file and extract width and create html snippet.
-        if ($process_flag) {
-            $image_width = $1;
-        }
-    } elsif ($line =~ m/^  pixelHeight: (\d+)/) {
-        
-        # This line is for the image height, so check we are processing the file and extract height and create html snippet.
-        if ($process_flag) {
-            $image_height = $1;
-            
-            # We have processed all the lines for the file so can process the data
-            # We need to work out which is the longer side and that is the one we constrain.  The other will be
-            # sized proportionally.
-            
-            if ($image_height >= $image_width) {
-                $size_string = "height='" . $max_dimension . "'";
-            } else {
-                $size_string = "width='" . $max_dimension . "'";
-            }
-
-            $tag_image = "<img " . " src='$image_path/$thumbnail_name' $size_string" . " />";
-            $html_line = $tag_td_left . "<a " . $href . ">" . $tag_image . $tag_anchor_right . $tag_td_right;
-            if ($output_lines == 0) {
-                $write .= $tag_table_left . "\n";
-            }
-            if ($output_lines % $table_column_width == 0) {
-                if ($output_lines != 0) {
-                    $write .= $tag_tr_right . "\n";
-                }
-                $write .= $tag_tr_left . "\n";
-            }
-            $write .= $html_line . "\n";
-            $output_lines++;
-        }
-    } else {
-        say "Unrecognised line $line, ignoring";
-    }
-}
-$write .= ($tag_td_left . $tag_td_right . "\n") x ($table_column_width - ($output_lines % $table_column_width));
-$write .= $tag_tr_right;
-$write .= $tag_table_right;
+# Make up page from gallery and template
 open my $file, '>', "gallery.shtml";
-$template->param('MAIN_CONTENT' => $write);
+my $template = HTML::Template->new(filename => 'gallery_template.html');
+$template->param('MAIN_CONTENT' => $page);
 print $file $template->output;
 close $file;
 
