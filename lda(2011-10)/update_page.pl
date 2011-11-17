42790876654ac3ec55d85abbff68648f408c1b90
Thomas Gaylard <26983554+sevire@users.noreply.github.com>
2011-11-17 07:49:15 +0000

    A bit more work on scripts to generate site.

--- lda(2011-10)/update_page.pl
+++ lda(2011-10)/update_page.pl
@@ -68,51 +68,7 @@ sub process_command_line {
 }
 
 sub main {
-    if (!WebGenHelper::page_exists($page_name)) {
-        die sprintf("Page <%s> does not exist\n", $page_name);
-    } else {
-        if (WebGenHelper::is_gallery($page_name)) {
-            say sprintf("<%s> is a gallery\n", $page_name);
-            # Creating a gallery so need to generate thumbnails / gallery HTML
-            
-        } else {
-            say sprintf("<%s> is not a gallery\n", $page_name);
-
-            # Make up page from text content and template
-            
-            my $template_name = $config{root} . $config{ds} . $config{template_folder} .
-                $config{ds} . $config{text_template_filename};
-            my $text_file_name = $config{root} . $config{ds} . $config{content_rel_path} . $config{ds} . $config{text_file_rel_path} .
-                $config{ds} . WebGenHelper::get_textfile_for_page($page_name);
-            my $target_fullpath = $config{target_root} . $config{ds} . $page_name . ".shtml";
-            
-            # Read in contents of text file
-            
-            my $text_file = WebGenHelper::get_textfile_for_page($page_name);
-            
-            my $handle;
-            my $holdTerminator = $/;
-            my $page_text;
-
-            undef $/;
-            
-            open($handle, $text_file_name) or die sprintf("Couldn't read text file <%s> for page <%s>", $text_file_name, $page_name);
-            $page_text = <$handle>; # Should slurp the file (read all contents in one go)
-            $/ = $holdTerminator;
-            print $page_text;
-            print "\n";
-            
-            # Pass template and text into Template to create complete page
-            
-            printf("Writing page <%s> to file <%s>\n", $page_name, $target_fullpath);
-            open my $file, '>', $target_fullpath;
-            my $template = HTML::Template->new(filename => $template_name);
-            $template->param('MAIN_CONTENT' => $page_text);
-            print $file $template->output;
-            close $file;
-
-        }
-    }
+    WebGenHelper::generate_page($page_name);
 }
 process_command_line;
 main;
