57d5a7e0c25686ea9c27cd2dca923b4b6a77b17b
Thomas Gaylard <26983554+sevire@users.noreply.github.com>
2011-12-08 08:11:00 +0000

    Lots of changes to scripts.  update_page.pl now working pretty well with both galleries and normal pages.  Thumbnails being created, table of images working.
    Also worked on templates and layout of page, which is going well.  Need more work to complete and also to ensue that it works on i.e..

--- lda(2011-10)/WebGenConfig.pm
+++ lda(2011-10)/WebGenConfig.pm
@@ -14,7 +14,9 @@ sub get_config_data {
         text_template_filename => "text_template.html",
         gallery_folder_rel_path => "galleries",
         text_file_pattern => "PAGE-(\\d{2})-(.+).txt", # Match file with format PAGE-nn-Pagename
-        table_column_width => 4
+        table_column_width => 4,
+        max_dimension => 130,
+        table_column_width => 4,
     );
     $data{text_rel_path} = $data{content_rel_path} . $data{ds} . "pages";
     $data{gallery_rel_path} = $data{content_rel_path} . $data{ds} . "galleries"; 
